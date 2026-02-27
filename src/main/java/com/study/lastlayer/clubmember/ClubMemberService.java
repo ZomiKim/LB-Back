d9b49563000faef27a101f625fc853874bf01ffb
jhr88 <uchung22@gmail.com>
2026-02-27 18:22:26 +0900

    0227 last

---
+++ src/main/java/com/study/lastlayer/clubmember/ClubMemberService.java
@@ -0,0 +1,35 @@
+package com.study.lastlayer.clubmember;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.study.lastlayer.member.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ClubMemberService {
+
+    private final ClubMemberRepository clubMemberRepository;
+
+    public List<ClubMemberDto> getClubsByMember(Member member) {
+        return clubMemberRepository.findByMemberId(member.getMember_id())
+            .stream()
+            .map(cm -> ClubMemberDto.builder()
+                    .clubId(cm.getClub().getId())
+                    .clubName(cm.getClub().getName())
+                    .clubDescription(cm.getClub().getDescription())
+                    .keywords(cm.getClub().getKeywords())
+                    .managerName(cm.getClub().getMember().getName())
+                    .bgFileId(cm.getClub().getFile() != null ? cm.getClub().getFile().getId() : null)
+                    .filename(cm.getClub().getFile() != null ? cm.getClub().getFile().getFilename() : null)
+                    .createdAt(cm.getClub().getCreatedAt())
+                    .build())
+            .collect(Collectors.toList());
+    }
+}
