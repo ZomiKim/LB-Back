7406614b822ec636dfe07449ba0aa06aece75ac7
hongjiseung <ghdwlgy1234@naver.com>
2026-03-03 17:15:50 +0900

    js 33

--- src/main/java/com/study/lastlayer/weekhistory/WeekHistoryDto.java
+++ src/main/java/com/study/lastlayer/weekhistory/WeekHistoryDto.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 
 public class WeekHistoryDto {
 	
-	public static record WeekHistoryRequest(String date, Double weight) {}
-	public static record WeekHistoryUpdateRequest(Double weight) {}
-	public static record WeekHistoryResponse(Long id, String date, Double weight, String message) {
+	public static record WeekHistoryRequest(String date, Float weight) {}
+	public static record WeekHistoryUpdateRequest(Float weight) {}
+	public static record WeekHistoryResponse(Long id, String date, Float weight, String message) {
 		public WeekHistoryResponse(WeekHistory entity, String message) {
 			this(entity.getId(), entity.getDate().toString(), entity.getWeight(), message);
 		}
 	}
 	
-	public static record WeekHistoryListResponse(Long id, String date, Double weight) {
+	public static record WeekHistoryListResponse(Long id, String date, Float weight) {
 		public WeekHistoryListResponse(WeekHistory entity) {
 			this(entity.getId(), entity.getDate().toString(), entity.getWeight());
 		}
